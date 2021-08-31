f4c06f25b77255052f30fea7d2898ece575ee635
kogoome <kogoome@kakao.com>
2021-08-31 17:15:24 +0900

    js server

--- src/api.js
+++ src/api.js
@@ -1,5 +1,4 @@
 // @ts-check
-// 주석은 기울임
 /**
  * @typedef Post
  * @property {string} id
@@ -7,19 +6,19 @@
  * @property {string} content
  */
 
-/** @type {Post[]} */
-const posts = [
-  {
-    id: 'my_first_post',
-    title: 'My first post',
-    content: 'Hello!',
-  },
-  {
-    id: 'my_second_post',
-    title: '나의 두번째 포스트',
-    content: 'Second post',
-  },
-]
+// /** @type {Post[]} */
+// const posts = [
+//   {
+//     id: 'my_first_post',
+//     title: 'My first post',
+//     content: 'Hello!',
+//   },
+//   {
+//     id: 'my_second_post',
+//     title: '나의 두번째 포스트',
+//     content: 'Second post',
+//   },
+// ]
 
 /**
  * Post : 글
@@ -43,6 +42,33 @@ const posts = [
  * @property {(matches: string[], body: Object.<string,*> | undefined) => Promise<APIResponse>} callback
  */
 
+const fs = require('fs')
+
+const DB_JSON_FILENAME = 'database.json'
+
+/**
+ * @returns {Promise<Post[]>}
+ */
+async function getPosts() {
+  const json = await fs.promises.readFile('database.json', 'utf-8')
+  return JSON.parse(json).posts
+}
+
+/**
+ * @param {Post[]} posts
+ */
+async function savePosts(posts) {
+  const content = {
+    posts,
+  }
+
+  return fs.promises.writeFile(
+    DB_JSON_FILENAME,
+    JSON.stringify(content),
+    'utf-8'
+  )
+}
+
 /** @type {Route[]} */
 const routes = [
   {
@@ -51,34 +77,36 @@ const routes = [
     callback: async () => ({
       // TODO: implement
       statusCode: 200,
-      body: posts,
+      body: await getPosts(),
     }),
   },
-
   {
     url: /^\/posts\/([a-zA-Z0-9-_]+)$/,
     method: 'GET',
     callback: async (matches) => {
       const postId = matches[1] // 첫번째 regexResult는 아이디값
-      if(!postId) { // 아이디가 없다면
+      if (!postId) {
+        // 아이디가 없다면
         return {
           statusCode: 200,
           body: 'Not found.',
         }
       }
-      
+      const posts = await getPosts()
       // 아이디가 일치하는 포스트 검색
-      const post = posts.find(_post => _post.id === postId)
-      if (!post) {// 포스트가 없다면
+      const post = posts.find((_post) => _post.id === postId)
+      if (!post) {
+        // 포스트가 없다면
         return {
           statusCode: 200,
           body: 'Not found.',
         }
       }
 
-      return {// 상태 번호와 해당아이디 포스트 반환
-        statusCode:200,
-        body:post,
+      return {
+        // 상태 번호와 해당아이디 포스트 반환
+        statusCode: 200,
+        body: post,
       }
     },
   },
@@ -86,24 +114,24 @@ const routes = [
   {
     url: /^\/posts$/,
     method: 'POST',
-    callback: async (_,body) => {
+    callback: async (_, body) => {
       if (!body) {
         return {
-          statusCode:400,
-          body: 'Ill-formed request.'
+          statusCode: 400,
+          body: 'Ill-formed request.',
         }
       }
 
-      /** @type {string} */
       /* eslint-disable-next-line prefer-destructuring */
-      const title = body.title
       const newPost = {
-        id:body.id,
-        title:body.title,
-        content:body.content,
+        id: body.id,
+        title: body.title,
+        content: body.content,
       }
 
+      const posts = await getPosts() // 기존 posts로 변수할당
       posts.push(newPost)
+      savePosts(posts)
       // TODO: implement
       return {
         statusCode: 200,
